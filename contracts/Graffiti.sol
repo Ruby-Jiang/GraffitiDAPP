35f77d282ba7ad915fa6cde34df1cfde733fbc56
RubyJ <xi.jiang@ucdconnect.ie>
2020-10-14 13:21:53 +0100

    Smoke Test

---
+++ contracts/Graffiti.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Graffiti{
+    // Store painter
+    // Read candidate
+    string public painter;
+    // Constructor
+    constructor() public {
+        painter = "painter 1";
+    }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var Graffiti = artifacts.require("./Graffiti.sol");
+
+module.exports = function (deployer) {
+  deployer.deploy(Graffiti);
+};
