712033d013e0e29a04473e82f0b1b93f4ca0c518
RubyJ <xi.jiang@ucdconnect.ie>
2020-10-15 20:48:18 +0100

    Update contract

--- contracts/Graffiti.sol
+++ contracts/Graffiti.sol
@@ -2,11 +2,26 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Graffiti{
-    // Store painter
-    // Read candidate
-    string public painter;
+    // Model a painter
+    struct Painter{
+        uint id;
+        string name;
+        string paintContent;
+    }
+    // Store Painters
+    // Fetch Painter
+    mapping(uint => Painter) public painters;
+    // Store Painters Count
+    uint public paintersCount;
     // Constructor
+
     constructor() public {
-        painter = "painter 1";
+        addPainter("Painter 1");
+        addPainter("Painter 2");
+    }
+
+    function addPainter (string memory _name) private{
+        paintersCount++;
+        painters[paintersCount] = Painter(paintersCount, _name, "");
     }
 }
