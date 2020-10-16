2cee94d5d18aede142968898393a08bcacf47138
RubyJ <xi.jiang@ucdconnect.ie>
2020-10-16 16:17:58 +0100

    Switch web3 provider

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,6 @@ App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
-  hasPainted: false,
 
   init: function() {
     return App.initWeb3();
@@ -12,12 +11,12 @@ App = {
     // TODO: refactor conditional
     if (typeof web3 !== 'undefined') {
       // If a web3 instance is already provided by Meta Mask.
-      App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
-    } else {
-      // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
     }
     return App.initContract();
   },
@@ -40,10 +39,11 @@ App = {
 
     loader.show();
     content.hide();
-
     // Load account data
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
+        var version = web3.version.api;
+        console.log(version)
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
       }
